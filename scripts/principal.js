6020e0a72e438de0b4f2fc8aa56e5a6a00131a2e
GiovannePDS7 <giovanne3282@gmail.com>
2025-05-19 14:43:45 -0300

    add: Adicionando arquivos de back-end

--- scripts/principal.js
+++
@@ -1,5 +0,0 @@
-var nomeUsuario = localStorage.getItem('nome')
-var emailUsuario = localStorage.getItem('email')
-var spanUsuario = document.getElementById('spanUsuario');
-
-spanUsuario.innerHTML = `Seja bem vindo(a) ${nomeUsuario}`
